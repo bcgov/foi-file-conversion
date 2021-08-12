94fa291f4dc20445c7b3a25724217bdcf780b094
Abin Antony <abin.antony@aot-technologies.com>
2021-08-11 17:58:35 -0700

    #322

--- MCS.FOI.FilePreProcessor/MCS.FOI.MSGAttachmentsToPdf/MSGFileProcessor.cs
+++ MCS.FOI.FilePreProcessor/MCS.FOI.MSGAttachmentsToPdf/MSGFileProcessor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using MsgReader.Outlook;
 using Serilog;
 namespace MCS.FOI.MSGAttachmentsToPdf
@@ -23,81 +24,107 @@ namespace MCS.FOI.MSGAttachmentsToPdf
             try
             {
                 string sourceFile = Path.Combine(MSGSourceFilePath, MSGFileName);
-                Dictionary<string,Object> problematicFiles = null;
-                using (var msg = new Storage.Message(sourceFile))
+                Dictionary<string, Object> problematicFiles = null;
+
+                if (File.Exists(sourceFile))
                 {
-                    var attachments = msg.Attachments;
-                    foreach (Object attachment in attachments)
+
+                    for (int attempt = 1; attempt < FailureAttemptCount; attempt++)
                     {
 
-                        var type = attachment.GetType().FullName;
-                     
-                        if(type.ToLower().Contains("message"))
+                        try
                         {
-                            var file = (Storage.Message)attachment;
-                            problematicFiles = problematicFiles == null ? new Dictionary<string, Object>() : problematicFiles;
-                            problematicFiles.Add(file.FileName, file);
-                                
-                        }
-                        else
-                        {
-                            var file = (Storage.Attachment)attachment;
-                            if (file.FileName.ToLower().Contains(".xls") || file.FileName.ToLower().Contains(".ics") || file.FileName.ToLower().Contains(".msg"))
+
+                            using (var msg = new Storage.Message(sourceFile))
                             {
-                                problematicFiles = problematicFiles == null ? new Dictionary<string, Object>() : problematicFiles;
-                                problematicFiles.Add(file.FileName, file);
-
-                            }
-
-                        }
-
-                    }
-
-                    if (problematicFiles != null)
-                    {
-                        int cnt = 0;
-                        foreach (var attachmenttomove in problematicFiles)
-                        {
-                            
-                            if (attachmenttomove.Key.ToLower().Contains(".msg"))
-                            {
-                                var _attachment = (Storage.Message)attachmenttomove.Value;
-                                string fileName = @$"{OutputFilePath}\msgattachments\{Path.GetFileNameWithoutExtension(MSGFileName)}_{_attachment.FileName}";
-                                foreach (var subattachment in _attachment.Attachments)
+                                var attachments = msg.Attachments;
+                                foreach (Object attachment in attachments)
                                 {
-                                    var type = subattachment.GetType().FullName;
-                                    if (type.ToLower().Contains("attachment"))
+
+                                    var type = attachment.GetType().FullName;
+
+                                    if (type.ToLower().Contains("message"))
                                     {
-                                        var file = (Storage.Attachment)subattachment;
-                                        CreateOutputFolder("subattachments");
-                                        string _fileName = @$"{OutputFilePath}\msgattachments\subattachments\{Path.GetFileNameWithoutExtension(MSGFileName)}_{file.FileName}";
-                                        File.WriteAllBytes(_fileName, file.Data);
+                                        var file = (Storage.Message)attachment;
+                                        problematicFiles = problematicFiles == null ? new Dictionary<string, Object>() : problematicFiles;
+                                        problematicFiles.Add(file.FileName, file);
+
+                                    }
+                                    else
+                                    {
+                                        var file = (Storage.Attachment)attachment;
+                                        if (file.FileName.ToLower().Contains(".xls") || file.FileName.ToLower().Contains(".ics") || file.FileName.ToLower().Contains(".msg"))
+                                        {
+                                            problematicFiles = problematicFiles == null ? new Dictionary<string, Object>() : problematicFiles;
+                                            problematicFiles.Add(file.FileName, file);
+
+                                        }
 
                                     }
 
                                 }
-                                
-                            }
-                            else
-                            {
-                                var _attachment = (Storage.Attachment)attachmenttomove.Value;
-                                string fileName = @$"{OutputFilePath}\msgattachments\{Path.GetFileNameWithoutExtension(MSGFileName)}_{_attachment.FileName}";
-                                CreateOutputFolder();
-                                File.WriteAllBytes(fileName, _attachment.Data);
-                                outputpath += fileName;
-                            }
 
-                       
-                            cnt++;
-                           
+                                if (problematicFiles != null)
+                                {
+                                    int cnt = 0;
+                                    foreach (var attachmenttomove in problematicFiles)
+                                    {
+
+                                        if (attachmenttomove.Key.ToLower().Contains(".msg"))
+                                        {
+                                            var _attachment = (Storage.Message)attachmenttomove.Value;
+                                            string fileName = @$"{OutputFilePath}\msgattachments\{Path.GetFileNameWithoutExtension(MSGFileName)}_{_attachment.FileName}";
+                                            foreach (var subattachment in _attachment.Attachments)
+                                            {
+                                                var type = subattachment.GetType().FullName;
+                                                if (type.ToLower().Contains("attachment"))
+                                                {
+                                                    var file = (Storage.Attachment)subattachment;
+                                                    CreateOutputFolder("subattachments");
+                                                    string _fileName = @$"{OutputFilePath}\msgattachments\subattachments\{Path.GetFileNameWithoutExtension(MSGFileName)}_{file.FileName}";
+                                                    File.WriteAllBytes(_fileName, file.Data);
+
+                                                }
+
+                                            }
+
+                                        }
+                                        else
+                                        {
+                                            var _attachment = (Storage.Attachment)attachmenttomove.Value;
+                                            string fileName = @$"{OutputFilePath}\msgattachments\{Path.GetFileNameWithoutExtension(MSGFileName)}_{_attachment.FileName}";
+                                            CreateOutputFolder();
+                                            File.WriteAllBytes(fileName, _attachment.Data);
+                                            outputpath += fileName;
+                                        }
+
+
+                                        cnt++;
+
+                                    }
+                                    moved = true;
+                                    message = string.Concat($"{cnt} problematic files moved", outputpath);
+                                }
+
+                                break;
+                            }
                         }
-                        moved = true;
-                        message = string.Concat($"{cnt} problematic files moved", outputpath);
+                        catch(Exception e)
+                        {
+                            message = $"Exception happened while accessing File {sourceFile}, re-attempting count : {attempt} , Error Message : {e.Message} , Stack trace : {e.StackTrace}";
+                            Log.Error(message);
+                            Console.WriteLine(message);                            
+                            Thread.Sleep(WaitTimeinMilliSeconds);
+                        }
                     }
-                   
+                }
+                else
+                {
+                    message = $"{sourceFile} does not exist!";
+                    Log.Error(message);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Log.Error($"Error happened while moving attachments on {MSGSourceFilePath}\\{MSGFileName} , Exception message : {ex.Message} , details : {ex.StackTrace}");
                 message = $"Error happened while moving attachments on {MSGSourceFilePath}\\{MSGFileName} , Exception message : {ex.Message} , details : {ex.StackTrace}";
@@ -114,7 +141,7 @@ namespace MCS.FOI.MSGAttachmentsToPdf
 
         private void CreateOutputFolder(string subpath = "")
         {
-            string msgfilefolder = string.Concat(OutputFilePath, @"\msgattachments",!string.IsNullOrEmpty(subpath) ? @$"\{subpath}" : "");
+            string msgfilefolder = string.Concat(OutputFilePath, @"\msgattachments", !string.IsNullOrEmpty(subpath) ? @$"\{subpath}" : "");
             if (!Directory.Exists(msgfilefolder))
                 Directory.CreateDirectory(msgfilefolder);
         }
